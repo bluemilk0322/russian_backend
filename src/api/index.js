7e4673f9ee0471d4674b1b8b1380a1dd81bfec6e
tommy1994111 <tommy1994111@gmail.com>
2018-01-10 20:37:50 +0800

    api 串接檔初步建立

--- src/api/index.js
+++ src/api/index.js
@@ -3,29 +3,86 @@ import axios from 'axios'
 const root = 'http://192.168.88.204:3030/api/'
 
 const link = {
-  navigation: root + 'navigation'
+  navigation: root + 'navigation',
+  navigationItem: 'navigation-item',
+  slider: root + 'slider',
+  carousel: root + 'carousel',
+  member: root + 'member',
+  content: root + 'content',
+  admission: root + 'admission',
+  banner: root + 'banner',
+  winning: root + 'winning',
+  news: root + 'news',
+  file: root + 'file',
+  user: root + 'user'
+}
+
+class method {
+  static get (link) {
+    return {
+      method: 'get',
+      url: link
+    }
+  }
+  static post (link, data) {
+    return {
+      method: 'post',
+      url: link,
+      data: data
+    }
+  }
 }
 
 export const api = {
   navigation: {
     get () {
-      const method = {
-        method: 'get',
-        url: link.navigation
-      }
-      return axios(method).then(response => {
+      return axios(method.get(link.navigation)).then(response => {
         return response.data
       })
     },
-    create (data) {
+    search ({ navigationId }) {
+      const requestLink = link.navigation + '/' + navigationId
+      return axios(method.get(requestLink)).then(response => {
+        return response.data
+      })
+    },
+    create ({ name }) {
+      const data = {
+        name: name
+      }
+      return axios(method.post(link.navigation, data)).then(response => {
+        return response
+      })
+    },
+    edit () {
       const method = {
         method: 'post',
         url: link.navigation,
-        data: data
+        data: {
+          name: name
+        }
+      }
+      return axios(method).then(response => {
+        return response
+      })
+    },
+    delete () {
+      const method = {
+        method: 'post',
+        url: link.navigation,
+        data: {
+          name: name
+        }
       }
       return axios(method).then(response => {
         return response
       })
     }
+  },
+  slider: {
+    get () {
+
+    }
   }
+
 }
