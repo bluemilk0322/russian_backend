1fa314ac3cdfdbfb09a3c36a57f93acfd2239d2e
tommy1994111 <tommy1994111@gmail.com>
2018-02-25 21:18:24 +0800

    廢棄vuex actions(root)

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,31 +1,5 @@
 import { types } from './mutations_types'
 
 export const actions = {
-  saveStatus ({ commit, state, dispatch }) {
-    localStorage.setItem('login_status', JSON.stringify(state.login))
-  },
-  loadStatus ({ commit, state, dispatch }) {
-    const status = JSON.parse(localStorage.getItem('login_status'))
-    commit(types.UPDATE_LOGIN_STATUS, status)
-  },
-  loginAction ({ commit, state, dispatch }, { account, password }) {
-    const accountMatched = account === 'admin'
-    const passwordMatched = password === 'admin1234'
-    if (accountMatched && passwordMatched) {
-      const status = {
-        session: 'logined',
-        user: 'admin'
-      }
-      commit(types.UPDATE_LOGIN_STATUS, status)
-      dispatch('saveStatus')
-    }
-  },
-  logoutAction ({ commit, state, dispatch }) {
-    const status = {
-      session: 'failed',
-      user: ''
-    }
-    commit(types.UPDATE_LOGIN_STATUS, status)
-    dispatch('saveStatus')
-  }
+
 }
